48bc8e07792b02b08df6b63562791d2c8b59de85
gameraxom88-afk <gameraxom88@gmail.com>
2026-01-27 20:56:38 +0530

    Update main_render.py

--- main_render.py
+++ main_render.py
@@ -1,1 +1,12 @@
- 
+@app.route("/manual", methods=["GET"])
+def manual():
+    sample_data = {
+        "symbol": "NIFTY",
+        "side": "BUY",
+        "qty": 1
+    }
+
+    handle_trade(sample_data)  # agar naam different ho, bata dena
+
+    return "MANUAL TRADE TRIGGERED" 
+
